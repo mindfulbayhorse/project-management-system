d763c1dc57e138f35c501dddb0d5f06821205657
Dasky Moorhen <mindfulbayhorse@gmail.com>
2020-05-19 18:41:21 +0300

    Removed old submodule

--- resources/modules/multilevel_structure/js/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Implemenration of WBS
- * Olga Zhilkova
- */
-import ko from "knockout";
-import ViewWBS from './viewmodels/wbs.js';
-
-let viewWBS = new ViewWBS([]);
-ko.applyBindings(viewWBS);

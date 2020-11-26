a91093c3887954605c55ebc37d472e111dddf53b
Olga Zhilkova <mindfulbayhorse@gmail.com>
2020-11-26 20:34:52 +0300

    Styled registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,51 +1,35 @@
 @extends('layout')
 
-@section('title','List of projects')
+@section('title','Registration')
 
 @section('content')
 
 @include('show_err')
 
-    <form name="authonticate" method="POST" action=""  class="groupped flex_block one_row">
+    <form name="authonticate" 
+        method="POST" 
+        action=""  
+        class="registration">
 
         @csrf
-	        
-	        <div class="flex_block one_column grid_rows col_1">
-	        
-	            <div class="flex_block one_column">
-	                <label for="title">User email:</label>
-	                <input type="text" name="email" value="{{ old('email') }}" />
-	            </div> 	
-	            	        
-	        </div>
-	        
-	        <div class="flex_block one_column grid_rows col_2">
+
+        <div class="field">
+            <label for="title">User email:</label>
+            <input type="text" name="email" value="{{ old('email') }}" />
+        </div> 	
+  
+        <div class="field">
+            <label for="cost">Password:</label>
+            <input type="password" name="password" value=""/>
+        </div>
 	            
-	            <div class="flex_block one_column">
-	                <label for="cost">Password:</label>
-	                <input type="password" name="password" value=""/>
-	            </div>
-	            
-	        </div>
-	        
-	          <div class="flex_block one_column grid_rows col_2">
-	            
-	            <div class="flex_block one_column">
-	                <label for="cost">Confirm password:</label>
-	                <input type="password" name="password_confirmation" value=""/>
-	            </div>
-	            
-	        </div>
-	        
-	         <div class="flex_block one_column grid_rows col_2">
-	            
-	            <div class="flex_block one_column">
-	        
-	                <input type="submit" name="login" value="Sing in" />
-	                
-	            </div>
-	            
-	         </div>
-    
+        <div class="field">
+            <label for="cost">Confirm password:</label>
+            <input type="password" name="password_confirmation" value=""/>
+        </div>
+        
+        <div class="field">
+            <input type="submit" name="login" value="Sing in" />   
+        </div>
     </form>
 @endsection

7f7a8bd6f2aab460f84a84ebd2844571ec789f08
Olga Zhilkova <mindfulbayhorse@gmail.com>
2020-11-26 08:26:40 +0300

    Project team cannot have more than one same members

--- tests/Unit/ProjectTeamTest.php
+++ tests/Unit/ProjectTeamTest.php
@@ -29,4 +29,17 @@ class ProjectTeamTest extends TestCase
         
         $this->assertCount(1, $this->project->team);
     }
+    
+    /** @test */
+    public function user_cannot_be_added_to_the_same_the_team_twice()
+    {
+        $this->withoutExceptionHandling();
+        $this->project->addMember($this->user);
+        
+        
+        $this->expectException('Exception');
+        $this->project->addMember($this->user);
+        
+        $this->assertCount(1, $this->project->team);
+    }
 }

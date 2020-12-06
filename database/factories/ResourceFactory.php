fbe95c2fbe9d8ae79fde793eda475e10e2f122a1
Olga Zhilkova <mindfulbayhorse@gmail.com>
2020-12-06 22:07:24 +0300

    Updated factory for resource based on database

--- database/factories/ResourceFactory.php
+++ database/factories/ResourceFactory.php
@@ -6,12 +6,18 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Resource;
 use Faker\Generator as Faker;
 use App\Models\User;
+use App\Models\ResourceType;
 
-$factory->define(Resource::class, function (Faker $faker) {
-    return [
-        'name'=> $faker->word,
-    	'type_id' => factory(ResouceType::Class)->create()->id,
-    	'valuable_id' => factory(User::Class)->create()->id,
-    	'valuable_type' => User::Class
-    ];
-});
+class ResourceFactory extends Factory
+{
+    protected $model = Resource::class;
+    
+    public function definition()
+    {
+        return [
+        	'type_id' => ResourceType::factory()->create()->id,
+        	'valuable_id' => User::factory()->create()->id,
+        	'valuable_type' => User::Class
+        ];
+    }
+}

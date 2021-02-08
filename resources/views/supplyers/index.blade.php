9f0846775683d01e6955eb7e79440948e569cc20
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-02-08 18:19:21 +0300

    Section for list of supplyers

---
+++ resources/views/supplyers/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+
+@section('title','List of projects')
+
+@section('content')
+<a href="/supplyers/create" class="btn">Add supplyer</a>
+<main>
+    <div class="projects_groups">
+        @forelse ($supplyers as $supplyer)
+                
+	        <div class="card">
+                <a href="{{$supplyer->path()}}" 
+                    class="title">{{$supplyer->name}}</a>
+                <div class="card-body">
+                    <div class="card-text">
+                        <p>{{ $supplyer->url }}</p>
+                    </div>
+                   
+                </div>
+	        </div>
+        @empty
+            <div>No projects have been added yet.</div>
+	    @endforelse
+    
+    </div>
+    
+</main>
+@endsection

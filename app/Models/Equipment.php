3f3a39aba68e179e2ef88532348a8c288605a70f
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-02-09 15:03:25 +0300

    Suppliers for equipment

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -5,12 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Suites\Resourcefulness;
+use App\Models\Supplier;
 
 class Equipment extends Model
 {
     use HasFactory, Resourcefulness;
     
     protected $table = 'equipment';
+    
+    protected $casts = [
+        'products_range' => 'array',
+    ];
     
     protected $guarded = [];
     
@@ -21,5 +26,10 @@ class Equipment extends Model
     public function type()
     {
         return $this->hasOne(ResourceType::class, 'id');
+    }
+    
+    public function suppliers()
+    {
+        return $this->morphToMany(Supplier::class, 'supply', 'supplies','supply_id', 'supplier_id');
     }
 }

f6d665e0b8f42895533fa810b505c8d29faf0398
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-02-19 19:42:31 +0300

    Database structure for users, roles and permissions

---
+++ database/migrations/2021_02_19_054248_create_roles_permissions_tables.php
@@ -0,0 +1,81 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesPermissionsTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function(Blueprint $table){
+            
+            $table->id();
+            $table->string('name');
+            $table->string('label');
+            $table->timestamps(6);
+            
+        });
+        
+        Schema::create('permissions', function(Blueprint $table){
+            
+            $table->id();
+            $table->string('name');
+            $table->string('label');
+            $table->timestamps(6);
+            
+        });
+        
+        Schema::create('users_roles', function(Blueprint $table){
+            
+            $table->primary(['user_id', 'role_id']);
+            
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            
+            $table->foreignId('role_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            
+            $table->timestamps(6);
+            
+        });
+        
+        
+        Schema::create('role_permission', function(Blueprint $table){
+            
+            $table->primary(['permission_id', 'role_id']);
+            
+            $table->foreignId('permission_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            
+            $table->foreignId('role_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            
+            $table->timestamps(6);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

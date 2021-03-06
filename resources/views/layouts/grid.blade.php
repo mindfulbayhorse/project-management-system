dc5db74f3108e5d286e43e73a7ad2d2518e38d9e
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-03-06 19:27:57 +0300

    Specific layout for sections that includes two clumns for content

---
+++ resources/views/layouts/grid.blade.php
@@ -0,0 +1,8 @@
+<x-projects.wbs>
+    <div class="part_left">
+         @yield('left_section') 
+    </div>
+    <div class="part_right">
+        @yield('right_section')
+    </div>
+</x-projects.wbs>

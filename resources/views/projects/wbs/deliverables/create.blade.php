af0e96197ed783a796f3f02698a8c0c53a6d6c54
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-03-08 19:33:26 +0300

    Customized edit deliverable page

--- resources/views/projects/wbs/deliverables/create.blade.php
+++ resources/views/projects/wbs/deliverables/create.blade.php
@@ -1,14 +1,69 @@
-@include('show_err')
+@extends('layouts.grid')
 
-<form id="deliverable" 
-    name="deliverable" 
-    method="POST" 
-    action="{{ $wbs->project->path() }}/deliverables"
-    class="deliverable new">
+@section('title','Create new deliverable')
 
-    <input type="hidden" name="wbs_id" value="{{ $wbs->id }}" />     
-        @include('projects.wbs.deliverables.form',  [
-                'deliverable' => new App\Models\Deliverable,
-                'btnTitle' => 'Create'
+@section('left_section')
+
+    @include('show_err')
+    
+    <form id="deliverable" 
+        name="deliverable" 
+        method="POST" 
+        action="{{ $project->path() }}/deliverables"
+        class="deliverable new">
+    
+        <input type="hidden" name="wbs_id" value="{{ $wbs->id }}" />     
+            @include('projects.wbs.deliverables.form',  [
+                    'deliverable' => new App\Models\Deliverable,
+                    'btnTitle' => 'Create'
+            ])
+    </form>
+@endsection
+
+@section('right_section')
+
+    <fieldset>
+        <legend>Arrange order</legend>
+        <div class="order_choice">
+            <div class="">
+              <input
+                type="radio" 
+                value="" 
+                form="deliverbale" 
+                id="last"
+                name="order_choice" >
+              <label class="form-check-label" for="last">
+                Insert first
+              </label>
+            </div>
+            <div class="">
+              <input
+                type="radio" 
+                value="" 
+                form="deliverbale" 
+                id="last"
+                name="order_choice" >
+              <label class="form-check-label" for="last">
+                Insert last
+              </label>
+            </div>
+            <div class="">
+              <input
+                type="radio" 
+                value="" 
+                form="deliverbale" 
+                id="last"
+                name="order_choice" >
+              <label class="form-check-label" for="last">
+                Insert next to:
+              </label>
+            </div>
+        </div>
+        
+        @include('projects.wbs.table',  [
+                'form' => 'deliverable',
         ])
-</form> 
+    </fieldset>
+    
+            
+@endsection
--- resources/views/projects/wbs/deliverables/form.blade.php
+++ resources/views/projects/wbs/deliverables/form.blade.php
@@ -5,75 +5,89 @@
 @endif
 
 <input type="hidden" name="project_id" value="{{$project->id}}" />
-  
-    <div class="form_section">
-        <div class="detail_field title">
-            <label for="title">Title: <span>*</span></label>
-                @error('title', 'deliverable')
-                    {{ $message }}
-                @enderror
-                <input type="text" 
-                    name="title" 
-                    id="title"
-                    value="{{ old('title', $deliverable->title)}}"
-                />
-        </div>
-        
-        <div class="complex">
-            <div class="detail_field double">
-                <label for="title">Cost:</label>
-                <input type="text" 
-                    name="cost" 
-                    id="cost"
-                    value="{{$deliverable->cost}}"
-                />
-            </div>  
-            
-             <div class="detail_field double">
-                <label for="order">Order:</label>
-                <input type="text" 
-                    name="order" 
-                    id="order"
-                    value="{{$deliverable->order}}"
-                />
-           </div>
-        </div>
+
+<fieldset class="form_section">
+    <legend>Basic information</legend>
+
+    <div class="detail_field title">
+        <label for="title">Title: <span>*</span></label>
+            @error('title', 'deliverable')
+                {{ $message }}
+            @enderror
+            <input type="text" 
+                name="title" 
+                id="title"
+                value="{{ old('title', $deliverable->title)}}"
+            />
+    </div>
+
+    <div class="detail_field double">
+        <label for="title">Cost:</label>
+        <input type="text" 
+            name="cost" 
+            id="cost"
+            value="{{$deliverable->cost}}"
+        />
+    </div>  
+
+    <div class="detail_field">
+        <label for="startDate"
+          id="startDateLabel"
+          tabindex="-1">Start date <span class="req">*<span>(required)</span></span></label>
+        <span>
+            <input type="date" 
+                class="form-control me-3"
+                id="startDate"
+                name="start_date"
+                required aria-required="true"
+                aria-labelledby="startDateLabel startDateFormat">
+            <span class="visually-hidden" 
+              id="startDateFormat" 
+              tabindex="-1">DD/MM/YYYY</span>
+        </span>
     </div>
     
-    <div class="form_section dates">
-        
-        <div class="detail_field">
-            @include('blocks.date', ['nameDate' => 'start_date', 'legend' => 'Start date'])
-        </div>
-        <div class="detail_field">
-          @include('blocks.date', ['nameDate' => 'end_date', 'legend' => 'End date'])
-        </div>
-    </div>  
+    <div class="detail_field">
+      <label for="endDate"
+          id="endDateLabel"
+          tabindex="-1">End date <span class="req">*<span>(required)</span></span></label>
+        <span>
+            <input type="date" 
+                class="form-control me-3"
+                id="endDate"
+                name="end_date"
+                required aria-required="true"
+                aria-labelledby="endDateLabel">
+            <span class="visually-hidden" 
+              id="endDateFormat" 
+              tabindex="-1">DD/MM/YYYY</span>
+        </span>
+    </div>
     
-     <div class="form_section">
-        <div class="complex">
-            <div class="detail_field_horizontal">
-                <input type="checkbox" 
-                    name="package" 
-                    id="package"
-                />
-                <label for="package" class="full_title">Package</label>
-            </div>
-            
-            <div class="detail_field_horizontal">
-                <input type="checkbox" 
-                    name="milestone" 
-                    id="milestone"
-                />
-                <label for="mistone" class="full_title">Milestone</label>
-            </div>
-        
-        </div>
-       
-       <div class="detail_field">  
-            <input type="submit" 
-                name='create' 
-                value="{{ $btnTitle}}"
+    <div class="complex">
+        <div class="detail_field_horizontal">
+            <input type="checkbox" 
+                name="package" 
+                id="package"
             />
+            <label for="package" class="full_title">Package</label>
         </div>
-     </div>  
+        
+        <div class="detail_field_horizontal">
+            <input type="checkbox" 
+                name="milestone" 
+                id="milestone"
+            />
+            <label for="mistone" class="full_title">Milestone</label>
+        </div>
+    
+    </div>
+   
+   <div class="detail_field">  
+        <input type="submit" 
+            name='create' 
+            value="{{ $btnTitle}}"
+        />
+    </div>
+
+</fieldset>

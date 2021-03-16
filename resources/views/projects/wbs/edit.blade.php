fb6ac03b5614a01c326f384e262370c41c0dbcf1
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-03-16 13:42:33 +0300

    New WBS section layout for project

--- resources/views/projects/wbs/edit.blade.php
+++ resources/views/projects/wbs/edit.blade.php
@@ -1,22 +1,23 @@
-@extends('layouts.grid')
+@extends('layouts.three_columns')
 
 @section('title','Work breakdown structure')
 
 @section('left_section')
-    @include('show_err')
-    
-    <div class="wbs-section-panel">
-        <a href="{{ route('projects.deliverables.create', $project) }}" 
-            class="create_new">Add new deliverable</a> 
 
-        @include('projects.activity.history')
-    </div>
+    @include('projects.wbs.deliverables.create')
+    
 @endsection
 
-@section('right_section')
+@section('center_section')
     <main>
          <h1>@yield('title')</h1>
          @include('projects.wbs.table')
     </main> 
+
+@endsection
+
+@section('right_sidebar')
+
+          @include('projects.activity.history') 
 
 @endsection

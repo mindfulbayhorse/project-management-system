383a3202f7f68ae673aeca0b9a0db55811b35039
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-03-16 13:43:47 +0300

    Changing order for deliverable

---
+++ resources/views/components/projects/wbs/deliverable/order.blade.php
@@ -0,0 +1,12 @@
+<fieldset>
+    <legend>Arrange order</legend>
+    <div class="order_choice">
+        <button>Make first</button>
+        <button>Ascern</button>
+        <button>Descern</button>
+        <button>Make last</button>
+    </div>
+    
+    {{ $slot }}
+    
+</fieldset>

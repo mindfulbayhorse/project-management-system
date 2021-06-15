42c7244e8d976c49b613df89e0035ce8c995fa2a
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-06-15 11:57:34 +0300

    New user table with token

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Carbon\Carbon;
+use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {

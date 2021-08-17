ea9dffea2c960a0ceb0738ebee46b73cb0bda148
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-08-17 11:44:13 +0300

    testing session after opened project

---
+++ tests/Integration/SessionProjectInterfaceTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Integration;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Project;
+use App\Models\User;
+
+class SessionProjectInterfaceTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    private $project;
+    
+    public $user;
+    
+    protected function setUp(): void 
+    {
+        parent::setUp();
+        
+        $this->project = Project::factory()->forManager()->create();
+        $this->signIn($this->project->manager);
+    }
+    
+    
+    /** @test */
+    public function show_last_open_project()
+    {
+        $response = $this->get(route('projects.show',['project'=>$this->project]));
+        $response->assertSessionHas('last_project', $this->project->id);
+    }
+}

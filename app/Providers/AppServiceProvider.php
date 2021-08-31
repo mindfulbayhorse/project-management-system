72a8fe12178147ab678b40c2e1850391215e4bbd
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-08-31 08:26:22 +0300

    showing last seen project on any view using widget

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Models\Project;
 use App\Models\Deliverable;
 use App\Observers\DeliverableObserver;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,7 +29,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        Blade::directive('widget', function($expression){
+            
+            $name = trim($expression);
+            return '<?= resolve("App\Http\Widgets\LastSeenProject")->loadView(); ?>';
+        });
+        
         Project::observe(ProjectObserver::class);
         Deliverable::observe(DeliverableObserver::class);
         DB::connection()->setQueryGrammar(new \App\Database\Query\Grammars\MySqlGrammar);

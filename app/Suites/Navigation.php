d1396f6c6bf0a263342d427bba6d1c1bb598e0df
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-11-02 22:50:13 +0300

    trying to remember page title in cache

--- app/Suites/Navigation.php
+++ app/Suites/Navigation.php
@@ -3,11 +3,18 @@ namespace App\Suites;
 
 use App\Models\SectionTitle;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cache;
 
 trait Navigation{
     
     public function getSection(){
         
-        return SectionTitle::where('code', Route::currentRouteName())->get()->first();
+        $section = cache()->remember('section.breadcrumbs', now()->addDay(), function () {
+            var_dump('new section title');
+            return SectionTitle::where('code', Route::currentRouteName())->get()->first();
+        });
+        
+        return $section;
+
     }
 }

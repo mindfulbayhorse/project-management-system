7e6a698fea13b0c7e18cfb9eb988e27c13d61edd
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-11-17 20:22:23 +0300

    unique generated name for Role's factory

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -22,7 +22,7 @@ class RoleFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->word,
+            'name' => $this->faker->unique()->word(),
             'label' => $this->faker->word
         ];
     }

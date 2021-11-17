fa4708be2b1f9eec568c499a66d87b9852ba8fe1
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-11-17 20:21:25 +0300

    testing validation role's name

--- tests/Feature/PriviledgesAdministratingTest.php
+++ tests/Feature/PriviledgesAdministratingTest.php
@@ -9,6 +9,8 @@ use App\Models\Role;
 
 class PriviledgesAdministratingTest extends TestCase
 {
+    use RefreshDatabase, WithFaker;
+    
     public $user;
     
     protected function setUp(): void
@@ -30,7 +32,7 @@ class PriviledgesAdministratingTest extends TestCase
         $response->assertStatus(200);
         
         $role = Role::factory()->make()->ToArray();
-        
+        //dd($role['name']);
         $saving = $this->post(route('roles.index'), $role);
 
         $saving->assertStatus(200);
@@ -78,7 +80,7 @@ class PriviledgesAdministratingTest extends TestCase
         
         $changes = [
             'id' => $role->id ,
-            'name' => 'new name',
+            'name' => 'new-name',
             'label' => 'new label'
         ];
         
@@ -98,4 +100,26 @@ class PriviledgesAdministratingTest extends TestCase
         $response = $this->get(route('roles.show',['role' => $role]));
         $response->assertStatus(200)->assertSee('<h1>'.$role->label.'</h1>', false);
     }
+    
+    
+    /** @test */
+    public function name_must_consist_of_symbols()
+    {
+        
+        $role = Role::factory()->make(['name'=>'admin2'])->toArray();
+        $response = $this->post(route('roles.index', ['role' => $role]));
+  
+        $response->assertSessionHasErrors(['name']);
+    }
+    
+    /** @test */
+    public function name_must_be_unique_through_table()
+    {
+      
+        Role::factory()->create(['name'=>'admin']);
+        $role2 = Role::factory()->make(['name'=>'admin'])->toArray();
+        $response = $this->post(route('roles.index', ['role' => $role2]));
+        $response->assertSessionHasErrors(['name']);
+    }
+    
 }

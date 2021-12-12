ad76bbd0461eeb31df2e37cbe4f8629b7154976b
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-12-12 20:17:59 +0300

    Creating admin user for first fresh DB

---
+++ database/seeders/UserAdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'email'=>'developer@gmail.com',
+            'password' => Hash::make(env('DEV_ADMIN_PASSWORD', 'password'))
+        ]);
+    }
+}

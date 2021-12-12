d87998289113ba8054f175211fc1b44282b03745
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-12-12 20:17:29 +0300

    Redis configuration with necessary password

--- config/database.php
+++ config/database.php
@@ -149,14 +149,14 @@ return [
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
+            'password' => env('REDIS_PASSWORD', "a_gQcsiOUS69cTqBTxAu"),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
 
         'cache' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
+            'password' => env('REDIS_PASSWORD', "a_gQcsiOUS69cTqBTxAu"),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],

a97e45b3cf564e6d1f85a689c9356b14a4adfdb3
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-12-14 20:25:54 +0300

    Relationships in supplies table with equipment and suppliers

--- app/Models/Supply.php
+++ app/Models/Supply.php
@@ -3,12 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Model;
+use Database\Factories\SupplyFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Supplies extends Pivot
+class Supply extends Pivot
 {
+    use HasFactory;
+    
     public $timestamps = false;
     
     protected $guarded = [];
     
     protected $table = 'supplies';
+    
+    
+    
 }

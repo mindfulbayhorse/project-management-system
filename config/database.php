2841b4f79d5155cc4b965122b05e5a1b4ead734d
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-12-14 20:56:15 +0300

    Deleting password for Redis in config file

--- config/database.php
+++ config/database.php
@@ -156,7 +156,7 @@ return [
 
         'cache' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', "a_gQcsiOUS69cTqBTxAu"),
+            'password' => env('REDIS_PASSWORD', ""),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],

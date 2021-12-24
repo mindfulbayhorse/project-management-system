5081270dc2c0e3dcec3d6460832e14b5fc624044
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-12-25 01:45:22 +0300

    Seeding for administrator if only he doesn't exist

--- database/seeders/UserAdminSeeder.php
+++ database/seeders/UserAdminSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Project;
+use App\Models\WorkBreakdownStructure;
+use App\Models\Deliverable;
 
 class UserAdminSeeder extends Seeder
 {
@@ -15,12 +18,15 @@ class UserAdminSeeder extends Seeder
      */
     public function run()
     {
-        
-        User::factory()->create([
-            'email'=>'developer@gmail.com',
-            'password' => Hash::make(env('DEV_ADMIN_PASSWORD')),
-            'email_verified_at' => now()
-        ]);
+
+        if (User::where('email','developer@gmail.com')->get()->count()==0){
+            User::factory()->create([
+                'email'=>'developer@gmail.com',
+                'password' => Hash::make(env('DEV_ADMIN_PASSWORD')),
+                'email_verified_at' => now()
+            ]);
+        }
+       
 
     }
 }

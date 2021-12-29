17192610f837147ba536d59a5f534f4fa6604eab
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-12-29 20:58:37 +0300

    Additional comments have been removed

---
+++ database/seeders/ProjectMainSeeder.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+use App\Models\Deliverable;
+use App\Models\WorkBreakdownStructure;
+use App\Models\Status;
+
+class ProjectMainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $this->seedProjectsWithStatus();
+        $this->seedMainProject();
+        
+    }
+    
+    function truncateStatuses() {
+        
+        $statuses = Status::all();
+        
+        foreach($statuses as $status){
+            if($status->projects){
+                Project::destroy($status->projects->pluck('id'));
+            }
+            $status->delete();
+        }
+        
+    }
+    
+    
+    function seedProjectsWithStatus() {
+        
+        $this->truncateStatuses();
+        $projects = Project::all();
+        
+        foreach($projects as $project){
+            $project->delete();
+        }
+
+        Project::factory()->for(Status::factory())
+            ->count(4)->create();     
+        
+    }
+    
+    function seedMainProject(){
+        
+        $status = Status::where(['name'=>'Initialized'])->first();
+        Project::where(['title' =>'Beadshine'])->delete();
+        
+        if($status){
+            $project = Project::factory([
+                'title'=>'Beadshine',
+                'status_id'=>$status->id
+            ])
+            ->create();
+        } else {
+            $project = Project::factory(['title'=>'Beadshine'])
+            ->for(Status::factory()->state(['name'=>'Initialized']))
+                ->create();
+        }
+        
+        Deliverable::factory()->state(['title'=>'Content'])
+        ->forWBS(['project_id'=>$project->id])
+        ->create();
+        
+    }
+}

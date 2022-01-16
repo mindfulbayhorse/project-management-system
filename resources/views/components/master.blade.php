fb77decd7e6fa41b76ac23b19b17bed28e92883c
Olga Zhilkova <mindfulbayhorse@gmail.com>
2022-01-16 23:42:57 +0300

    Including Alpine scripts library for UX

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -4,8 +4,8 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="/css/normalize.css" />
 <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}" />
+<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 <title>@yield('title','Laracast')</title>
 </head>
 <body>

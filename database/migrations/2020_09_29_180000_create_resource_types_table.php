24d8ccc30cddbc265185d5c1cfffd6ca18312991
Olga Zhilkova <mindfulbayhorse@gmail.com>
2022-02-09 23:42:38 +0300

    Updated table structure for resource type

--- database/migrations/2020_09_29_180000_create_resource_types_table.php
+++ database/migrations/2020_09_29_180000_create_resource_types_table.php
@@ -16,6 +16,7 @@ class CreateResourceTypesTable extends Migration
         Schema::create('resource_types', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('slug');
             $table->timestamps();
         });
     }

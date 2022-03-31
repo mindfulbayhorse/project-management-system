e481b4eace1f99f52c7be070f71e0457fd6cdd67
Olga Zhilkova <mindfulbayhorse@gmail.com>
2022-03-31 22:12:24 +0300

    Form for filtering projects by status and title

--- resources/views/components/projects/status-dropdown-list.blade.php
+++ resources/views/components/projects/status-dropdown-list.blade.php
@@ -4,10 +4,13 @@
 <x-forms.dropdown.dropdown :id="'status_filter'" :buttonText="$buttonStatus">
     @foreach($statuses as $status)
         @php
-            $link = route('projects.index',[], false).'?status='.$status->id
+            $link = route('projects.index',[], false).'?status='.$status->id;
+            $filter = http_build_query(request()->except('status'));
+            $params = '';
+            if ($filter) $params = '&'.$filter;
         @endphp
         <x-forms.dropdown.item 
-            href="{{ $link }}" 
+            href="{{ $link }}{{ $params }}" 
             :selected="request()->is($link)">{{ $status->name }}</x-forms.dropdown-item>
     @endforeach
 </x-forms.dropdown>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,32 +2,38 @@
 
     @section('title','List of projects')
 
-        <form method="GET" style="display: flex;">
-        
-            <a href="/projects/create" class="btn">Add project</a>
+        <a href="/projects/create" class="btn">Add project</a>
            @php
             $buttonText = isset($viewChoice[$currentView]) ? $viewChoice[$currentView] : 'By cards';
            @endphp
-       
-            <x-forms.dropdown.dropdown :id="'type_view'" :buttonText="$buttonText">
-            @foreach($viewChoice as $name => $view)
-               @if (isset($currentView) && $name !== $currentView)
-                @php
-                    $link = route('projects.index',[], false).'?showby='.$name
-                @endphp
-                <x-forms.dropdown.item 
-                    href="{{ $link }}" 
-                    :selected="request()->is($link)">{{ $view }}</x-forms.dropdown-item>
-               @endif
-            @endforeach
-        </x-forms.dropdown>
-        
-        <x-status-dropdown-list />
     
+        <x-forms.dropdown.dropdown :id="'type_view'" :buttonText="$buttonText">
+        @foreach($viewChoice as $name => $view)
+           @if (isset($currentView) && $name !== $currentView)
+            @php
+                $link = route('projects.index',[], false).'?showby='.$name
+            @endphp
+            <x-forms.dropdown.item 
+                href="{{ $link }}" 
+                :selected="request()->is($link)">{{ $view }}</x-forms.dropdown-item>
+           @endif
+        @endforeach
+    </x-forms.dropdown>
+    
+    <x-status-dropdown-list />
+   
+   <form method="GET" style="display: flex;" action="">        
+        
+              
+        @if (request('status'))
+            <input type="hidden" name="status" value="{{ request('status') }}" /> 
+        @endif
+        
         <div>
             <label for="search_title">Title:</label>
-            <input type="text" value="{{ request('title') }}" name="title"/>
+            <input type="text" value="{{ request('title') }}" name="title" />
         </div>
+  
         
     </form>
     

437de70a0f3e9eb2a165103f548f5f33f16e47ac
Olga Zhilkova <mindfulbayhorse@gmail.com>
2022-06-09 20:29:43 +0300

    Resources filter

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -34,11 +34,6 @@ class Resource extends Model
 	{
 	
 	    $query->whereHasMorph('valued', $type);
-    	/*$query->with(['valued' => function (MorphTo $morphTo) {
-    	        $morphTo->morphWith([
-    	            Equipment::class => ['valuable']
-    	        ]);
-    	    }]);*/
 	      
         $query->when($filter['type'] ?? false, fn($query, $slug)=>
             
@@ -49,56 +44,22 @@ class Resource extends Model
 	                    ->where('resource_types.slug', '=',$slug)
 	                    ->whereColumn('resource_types.id', '=','resources.type_id')
 	                )
-                /*->with(['valued' => function (MorphTo $morphTo) {
-	                    $morphTo->morphWith([
-	                        Equipment::class => ['valuable']
-	                    ]);
-	                }])*/
                 )
             
         );
         
-        //dd($filter); 
-	    /*
-	    $query->when($filter['type'] ?? false, function ($query, $resourceType) use ($type){
-
-	        $query->whereHasMorph('valued', $type, function($query) use ($type, $resourceType){
-                
-                $query->where('type_id', $resourceType);
-            });
-	    });
-
-        $query->when($filter['type'] ?? false, fn ($query, $resourceSlug)=>(
-            $query->whereExists(function ($query) {
-                var_dump($resourceSlug);
-                $query->select()
-                ->from('resource_types')
-                ->whereColumn('resource_types.slug', $resourceSlug);
-            })
-        ));*/
- 
-	    
-	    /*$query->when($filter['name'] ?? false, function ($query, $name) use ($type){
-
-	        $query->whereHasMorph('valued', $type, function($query) use ($type, $name){
-    	        
-               $column = $type === Equipment::class ? 'model' : 'name';
-               
-               $query->where($column, 'like', '%'.$name.'%');
-    	    });
-	    });*/
-	    
-	    /*$query->with(['valued' => function (MorphTo $morphTo) {
-	            $morphTo->morphWith([
-	                Equipment::class => ['valuable']
-	            ]);
-	        }]);*/
         
-        /*$query->with(['valued' => fn ($morphTo)=>
-            $morphTo->morphWith([
-                Equipment::class => ['valuable']
-            ])
-        ]);*/
+        $query->when($filter['model'] ?? false, function ($query, $name) use ($type) {
+            
+            $query->whereHasMorph('valued', $type, function($query) use ($type, $name){
+                
+                $column = $type === Equipment::class ? 'model' : 'name';
+                
+                $query->where($column, 'like', '%'.$name.'%');
+            });
+        });
+        
+       
 	}
 
 }

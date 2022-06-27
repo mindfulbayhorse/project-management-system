36e2a2f71871006bcd99248fc43bda358eb047f8
Olga Zhilkova <mindfulbayhorse@gmail.com>
2022-06-27 16:28:43 +0300

    Display successful message after creating status

--- resources/views/projects/statuses/edit.blade.php
+++ resources/views/projects/statuses/edit.blade.php
@@ -4,6 +4,10 @@
 
 @section('content')
 
+
+@if (session()->has('success'))
+    {{ session()->get('success') }}
+@endif
 <main>
     <form method="POST" action="{{$status->path()}}" class="status">
         @method('PATCH')
